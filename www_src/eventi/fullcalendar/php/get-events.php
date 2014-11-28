d761b09a7b31d004b04efe5a6b62432c6c94eb75
infame46 <bogdan.alexis@gmail.com>
2014-11-28 21:13:47 +0100

    Update get-events.php

--- www_src/eventi/fullcalendar/php/get-events.php
+++ www_src/eventi/fullcalendar/php/get-events.php
@@ -3,22 +3,22 @@ $output_arrays=array(
     array(
 		"id" => "1",
 		"title"=>"Evento 1",
-		"start"=>date(DATE_ATOM, mktime(0, 0, 0, 7, 1, 2015)),
-		"end"=>date(DATE_ATOM, mktime(0, 0, 0, 7, 1, 2015)),
+		"start"=>date(DATE_ATOM, mktime(0, 0, 0, 11, 30, 2014)),
+		"end"=>date(DATE_ATOM, mktime(0, 0, 0, 11, 30, 2014)),
 		"url"=>"google.it"
 		),
     array(
 		"id" => "2",
 		"title"=>"Evento 2",
-		"start"=> date(DATE_ATOM, mktime(0, 0, 0, 7, 1, 2015)),
-		"end"=>date(DATE_ATOM, mktime(0, 0, 0, 7, 1, 2015)),
+		"start"=> date(DATE_ATOM, mktime(0, 0, 0, 12, 1, 2014)),
+		"end"=>date(DATE_ATOM, mktime(0, 0, 0, 12, 1, 2014)),
 		"url"=>"google.it"
 		),
 	 array(
 		"id" => "3",
 		"title"=>"Evento 3",
-		"start"=> date(DATE_ATOM, mktime(0, 0, 0, 7, 1, 2015)),
-		"end"=>date(DATE_ATOM, mktime(0, 0, 0, 7, 1, 2015)),
+		"start"=> date(DATE_ATOM, mktime(0, 0, 0, 12, 4, 2015)),
+		"end"=>date(DATE_ATOM, mktime(0, 0, 0, 12, 4, 2015)),
 		"url"=>"google.it"
 		)
 );
