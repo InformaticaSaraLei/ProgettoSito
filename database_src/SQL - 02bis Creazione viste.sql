02fb677cabdcf2d6cff8b7169243699afe41526e
Antonio Bucciol <antonio@bucciol.org>
2014-11-28 20:36:51 +0100

    Aggiunti script SQL per creazione DB

---
+++ database_src/SQL - 02bis Creazione viste.sql
@@ -0,0 +1,35 @@
+DROP VIEW IF EXISTS vRSSFEEDS;
+
+CREATE VIEW vRSSFEEDS
+AS
+	SELECT
+		ID,
+        "esperienzedonne" AS nometab,
+		"Nuova esperienza di una donna" AS news,
+		concat(nome, ' ', cognome, ": donna da ", RETRIBUZIONE, " - La sua storia: ", MOTIVO_NOTORIETA) AS snippet
+	FROM
+		esperienzedonne
+UNION
+	SELECT
+		ID,
+		"opportunitaculturalipersone" AS nometab,
+		"Nuova opportunità culturale" AS news,
+		concat(nome, ' ', cognome, ' - ', RUOLO_ATTUALE, ' presso ', AZIENDA_ATTUALE) as snippet
+	FROM
+		opportunitaculturalipersone
+UNION
+	SELECT
+		ol.ID,
+        "offertelavoro" AS nometab,
+		"Nuova offerta di lavoro" AS news,
+        concat(ol.AZIENDA_NOME, ' ricerca: ', ol.TITOLO_LAVORO, ' per sede di ', ol.AZIENDA_CITTA, '(', ol.AZIENDA_PROVINCIA, ') - ', n.DESCRIZIONE) as snippet
+	FROM
+		offertelavoro ol
+        LEFT JOIN nazioni n ON ol.FK_NAZIONE = n.ID;
+
+
+/*
+
+select * from vRSSFEEDS;
+
+*/
