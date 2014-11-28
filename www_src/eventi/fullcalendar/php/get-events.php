09369a4b24f2b5079eff73fa8eb6bdb6685370e9
infame46 <bogdan.alexis@gmail.com>
2014-11-28 21:12:22 +0100

    Update get-events.php

--- www_src/eventi/fullcalendar/php/get-events.php
+++ www_src/eventi/fullcalendar/php/get-events.php
@@ -2,21 +2,21 @@
 $output_arrays=array(
     array(
 		"id" => "1",
-		"title"=>"Jopa",
+		"title"=>"Evento 1",
 		"start"=>date(DATE_ATOM, mktime(0, 0, 0, 7, 1, 2015)),
 		"end"=>date(DATE_ATOM, mktime(0, 0, 0, 7, 1, 2015)),
 		"url"=>"google.it"
 		),
     array(
 		"id" => "2",
-		"title"=>"Pizda",
+		"title"=>"Evento 2",
 		"start"=> date(DATE_ATOM, mktime(0, 0, 0, 7, 1, 2015)),
 		"end"=>date(DATE_ATOM, mktime(0, 0, 0, 7, 1, 2015)),
 		"url"=>"google.it"
 		),
 	 array(
 		"id" => "3",
-		"title"=>"Pula",
+		"title"=>"Evento 3",
 		"start"=> date(DATE_ATOM, mktime(0, 0, 0, 7, 1, 2015)),
 		"end"=>date(DATE_ATOM, mktime(0, 0, 0, 7, 1, 2015)),
 		"url"=>"google.it"
