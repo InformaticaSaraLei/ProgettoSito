7b82a44d789748dda9fddcc0728113f6ddc0b7d4
infame46 <bogdan.alexis@gmail.com>
2014-11-28 21:18:16 +0100

    Update get-events.php

--- www_src/eventi/fullcalendar/php/get-events.php
+++ www_src/eventi/fullcalendar/php/get-events.php
@@ -1,4 +1,44 @@
 <?php
+/*
+require_once "../lib/evento_original.php"
+// Require our Event class and datetime utilities
+require dirname(__FILE__) . '/utils.php';
+
+// Short-circuit if the client did not give us a date range.
+if (!isset($_GET['start']) || !isset($_GET['end'])) {
+	die("Please provide a date range.");
+}
+
+// Parse the start/end parameters.
+// These are assumed to be ISO8601 strings with no time nor timezone, like "2013-12-29".
+// Since no timezone will be present, they will parsed as UTC.
+$range_start = parseDateTime($_GET['start']);
+$range_end = parseDateTime($_GET['end']);
+
+// Parse the timezone parameter if it is present.
+$timezone = null;
+if (isset($_GET['timezone'])) {
+	$timezone = new DateTimeZone($_GET['timezone']);
+}
+$manager=new EventiManager;
+$input_arrays = $manager->getEventiAssoc();
+
+// Accumulate an output array of event data arrays.
+$output_arrays = array();
+foreach ($input_arrays as $array) {
+
+	// Convert the input array into a useful Event object
+	$event = new Event($array, $timezone);
+
+	// If the event is in-bounds, add it to the output
+	if ($event->isWithinDayRange($range_start, $range_end)) {
+		$output_arrays[] = $event->toArray();
+	}
+}
+
+// Send JSON to the client.
+echo json_encode($output_arrays);
+*/
 $output_arrays=array(
     array(
 		"id" => "1",
