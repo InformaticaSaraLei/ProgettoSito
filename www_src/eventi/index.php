d0b5e4a764a921840ed8bff196481b9a5b141745
Michele Minighin <m.minighin@gmail.com>
2014-12-05 12:02:27 +0100

    Aggiornamento index.php
    Cambiato il messaggio in caso di nessun evento da "Non evento programmato" a "Nessun evento programmato".
    Piccola correzione

--- www_src/eventi/index.php
+++ www_src/eventi/index.php
@@ -128,7 +128,7 @@ include_once "lib/evento.php";
     }else{
         echo '<div class="row"><div class="pull-left col-md-4 col-lg-4"></div>';
         echo '<div class="pull-left col-md-4 col-lg-4">';
-        echo '<p>Non evento programmato</p></div>';
+        echo '<p>Nessun evento programmato</p></div>';
         echo '<div class="pull-left col-md-4 col-lg-4"></div></div>';
     }
     
