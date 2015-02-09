ab90e050a71def7c368ef8de4d0971b1572e5028
Matteo Scarpa <matteoscarpa92@gmail.com>
2015-02-09 22:05:04 +0100

    Rese autoridimensionanti i div
    Modificati i div di racconti e about che si autoridimensionano in base alle necessità

--- www_src/admin/index.php
+++ www_src/admin/index.php
@@ -2,9 +2,9 @@
 //pagina di aiuto per far puntare il browsere o al login oppure alla pagina amministrativa
 //a seconda dello stato di autenticazione
 session_start();
-if(isset($_SESSION['login'])){
+if (isset($_SESSION['login'])) {
     header("Location: profile.php");
-    
+
 } else {
     header("Location: login.php");
 }
