3dcbcdb6fc07d90f8dd7178bf0c6f8e2d049103f
Matteo Scarpa <matteoscarpa92@gmail.com>
2015-02-11 17:57:56 +0100

    Gestito bug su Chrome

--- www_src/js/resize.js
+++ www_src/js/resize.js
@@ -12,13 +12,17 @@ function caller() {
     elaborator(".box6");
 }
 function elaborator(class_string) {
+    try {
     var newheight = 0;
-    var padding = ($(class_string).css("padding")).split("px");
+        var padding = $(class_string).css("padding").split("px");
     $(class_string).each(function () {
         if ($(this).height() > newheight) {
             newheight = $(this).height();
         }
     });
     var compHeight = newheight + 10 + "px";
-    $(class_string).css("height", compHeight);
+        $(class_string).css("height", compHeight);
+    }
+    catch (e) {
+    }
 }
