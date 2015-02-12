eb8c28e8bf629c995bdaed2796e4a72644566518
Leonardo Zuffellato <lzuffellato@users.noreply.github.com>
2015-02-12 19:10:57 +0100

    Update deleteNewsletter.php

--- www_src/servizi/deleteNewsletter.php
+++ www_src/servizi/deleteNewsletter.php
@@ -4,7 +4,7 @@ $servername = SETTINGS_DBHOST; /*da completare con i dati corretti del db*/
 $username = SETTINGS_USERNAME;
 $password = SETTINGS_PASSWORD;
 $dbname = SETTINGS_DATABASE;
-$emailNE = $_POST["formEmail"];        /*il form dovrà avere il campo dove l'utente inserisce l'email con name="FormEmail"*/
+$emailNE = $_POST["formEmail2"];        /*il form dovrà avere il campo dove l'utente inserisce l'email con name="FormEmail"*/
 $email= mysql_real_escape_string($emailNE);
 
 $conn = new mysqli($servername, $username, $password, $dbname);
