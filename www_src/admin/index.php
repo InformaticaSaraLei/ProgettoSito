4845e5b05278b7bd6baf6db8120b39e1e3ec957a
Pierluigi Tonegato <817866@stud.unive.it>
2015-02-14 00:04:57 +0100

    test

--- www_src/admin/index.php
+++ www_src/admin/index.php
@@ -1,9 +1,9 @@
 <?php
-//pagina di aiuto per far puntare il browsere o al login oppure alla pagina amministrativa
+//pagina di aiuto per far puntare il browser o al login oppure alla pagina amministrativa
 //a seconda dello stato di autenticazione
 session_start();
 if (isset($_SESSION['login'])) {
-    header("Location: profile.php");
+    header("Location: dashboard.php");
 
 } else {
     header("Location: login.php");
