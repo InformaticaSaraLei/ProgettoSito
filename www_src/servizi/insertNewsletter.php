669cd0cc3d5ab13ee091cbf229b1b6e239bccc67
Leonardo Zuffellato <lzuffellato@users.noreply.github.com>
2015-02-16 20:14:08 +0100

    Update insertNewsletter.php

--- www_src/servizi/insertNewsletter.php
+++ www_src/servizi/insertNewsletter.php
@@ -19,11 +19,11 @@ $sql = "INSERT INTO newsletterusers (email) VALUES ('$email')";
 
 if ($conn->query($sql) === TRUE) {
   header( "refresh:5;url=newsletter.html" );
-  echo "Iscrizione effettuata con successo!";
+  echo "Iscrizione effettuata con successo! Tra 5 secondi verrai reindirizzato alla pagina precedente!";
 } else {
   header( "refresh:5;url=newsletter.html" );
   echo "errore: controlla di avere inserito correttamente la tua mail, o di non esserti già iscritto!";
 //echo "Error: " . $sql . "<br>" . $conn->error;
-
+}
 $conn->close();
 ?> 
