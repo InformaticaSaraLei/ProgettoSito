31afd52bb6c209e5b3fa19007fe839ff3d7b9d26
Pierluigi Tonegato <817866@stud.unive.it>
2015-02-18 00:18:41 +0100

    bugfix su insert e delete mail da newsletter

--- www_src/servizi/deleteNewsletter.php
+++ www_src/servizi/deleteNewsletter.php
@@ -1,24 +1,42 @@
 <?php
+
 include_once '../settings.php';
-$servername = SETTINGS_DBHOST; /*da completare con i dati corretti del db*/
+$servername = SETTINGS_DBHOST; /* da completare con i dati corretti del db */
 $username = SETTINGS_USERNAME;
 $password = SETTINGS_PASSWORD;
 $dbname = SETTINGS_DATABASE;
-$emailNE = $_POST["formEmail2"];        /*il form dovrà avere il campo dove l'utente inserisce l'email con name="FormEmail"*/
-$email= mysql_real_escape_string($emailNE);
+$emailNE = $_POST["formEmail2"];        /* il form dovrà avere il campo dove l'utente inserisce l'email con name="FormEmail" */
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
-die("Connection failed: " . $conn->connect_error);
+    die("Connection failed: " . $conn->connect_error);
 }
-$sql = "DELETE FROM newsletterusers WHERE email= ('$email')";
-if ($conn->query($sql) === TRUE) {
-  header( "refresh:5;url=newsletter.html" );
-  echo "Utente cancellato con successo! Tra 5 secondi verrai reindirizzato alla pagina precedente!";
+$email = mysqli_real_escape_string($conn, $emailNE);
+if (!$email) {
+    die("impossibile eseguire il parsing della stringa: " . $conn->connect_error);
+}
+// la query di delete ti restituisce TRUE anche se cancella 0 righe; quindi gestisco 
+// la presenza della mail da cancellare..
+$sqls = "SELECT COUNT(*) as trovato FROM newsletterusers WHERE email= ('$email')";
+$sqld = "DELETE FROM newsletterusers WHERE email= ('$email')";
+$trovato = false;
+if (($ris = $conn->query($sqls))) {
+    $o = $ris->fetch_object();
+    if ($o->trovato)
+        $trovato = true;
+}
+if (!$trovato) {
+    header("refresh:5;url=newsletter.html");
+    echo "Utente non presente in archivio! Tra 5 secondi verrai reindirizzato alla pagina precedente!";
 } else {
-  header( "refresh:5;url=newsletter.html" );
-  echo "errore: controlla di avere inserito correttamente la mail da rimuovere, o di non esserti già cancellato!";
+    if ($conn->query($sqld) === TRUE) {
+        header("refresh:5;url=newsletter.html");
+        echo "Utente cancellato con successo! Tra 5 secondi verrai reindirizzato alla pagina precedente!";
+    } else {
+        header("refresh:5;url=newsletter.html");
+        echo "errore: controlla di avere inserito correttamente la mail da rimuovere, o di non esserti già cancellato!";
 //echo "Error: " . $sql . "<br>" . $conn->error;
+    }
 }
 $conn->close();
 ?> 
--- www_src/servizi/insertNewsletter.php
+++ www_src/servizi/insertNewsletter.php
@@ -5,7 +5,6 @@ $username = SETTINGS_USERNAME;
 $password = SETTINGS_PASSWORD;
 $dbname = SETTINGS_DATABASE;
 $emailNE = $_POST["formEmail"];        /*il form dovrà avere il campo dove l'utente inserisce l'email con name="FormEmail"*/
-$email= mysql_real_escape_string($emailNE);
 
 /*connessione al db, da sistemare*/
 
@@ -13,10 +12,12 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-
-
-$sql = "INSERT INTO newsletterusers (email) VALUES ('$email')";
-
+//eseguo l'escape SOLO dopo la connessione al DB..
+$email= mysqli_real_escape_string($conn,$emailNE);
+if(!$email) {
+    die("impossibile eseguire il parsing della stringa: " . $conn->connect_error);
+}
+$sql = "INSERT INTO newsletterusers (EMAIL) VALUES ('$email')";
 if ($conn->query($sql) === TRUE) {
   header( "refresh:5;url=newsletter.html" );
   echo "Iscrizione effettuata con successo! Tra 5 secondi verrai reindirizzato alla pagina precedente!";
