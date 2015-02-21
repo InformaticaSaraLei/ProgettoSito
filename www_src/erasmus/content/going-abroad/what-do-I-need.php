64e56d9f41b5be046b0cfcd4c196bfc703a567a6
Matteo Scarpa <matteoscarpa92@gmail.com>
2015-02-21 08:58:56 +0100

    Tentativo video responsive

--- www_src/erasmus/content/going-abroad/what-do-I-need.php
+++ www_src/erasmus/content/going-abroad/what-do-I-need.php
@@ -1,10 +1,14 @@
 <?php
 ?>
-<div>
-    <p>If you are interested in doing Erasmus, contact the international relations office of your home university and
-        ask about the possibilities to go study abroad through the ERASMUS programme.</p></div>
-<div class="text-center">
-    <object width="425" height="350" data="http://www.youtube.com/v/HgbKr1xtQqw" type="application/x-shockwave-flash">
-        <param name="src" value="http://www.youtube.com/v/HgbKr1xtQqw"/>
-    </object>
+<div class="container modal-body">
+    <div class="col-xs-12">
+        <p>If you are interested in doing Erasmus, contact the international relations office of your home university
+            and
+            ask about the possibilities to go study abroad through the ERASMUS programme.</p>
+    </div>
+    <div class="rwd-video col-xs-12">
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/HgbKr1xtQqw" frameborder="0"
+                allowfullscreen></iframe>
+    </div>
 </div>
+
