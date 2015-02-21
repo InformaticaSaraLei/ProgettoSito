6a8d57c3e7fe36dd831790dae2ba10e0bf8e78d7
Antonio Bucciol <antonio@bucciol.org>
2015-02-21 18:43:35 +0100

    Aggiunti campi a tabelle agendaeventi e offertelavoro. Rimosse tabelle opportunitaculturalipersone e opportunitaculturalicarriera. Aggiornata vista RSS: rimosse opportunitaculturali, aggiunti eventi.

--- database_src/SQL - 02bis Creazione viste.sql
+++ database_src/SQL - 02bis Creazione viste.sql
@@ -2,24 +2,27 @@ DROP VIEW IF EXISTS `pariopp`.vRSSFEEDS;
 
 CREATE VIEW `pariopp`.vRSSFEEDS
 AS
-	SELECT
-		ID,
-		"opportunitaculturalipersone" AS nometab,
-		"Nuova opportunità culturale" AS news,
-		concat(nome, ' ', cognome, ' - ', RUOLO_ATTUALE, ' presso ', AZIENDA_ATTUALE) as snippet,
-		DATA_INSERIMENTO AS data
-	FROM
-		`pariopp`.opportunitaculturalipersone
-UNION
+
 	SELECT
 		ol.ID,
         "offertelavoro" AS nometab,
 		"Nuova offerta di lavoro" AS news,
-        concat(ol.AZIENDA_NOME, ' ricerca: ', ol.TITOLO_LAVORO, ' per sede di ', ol.AZIENDA_CITTA, '(', ol.AZIENDA_PROVINCIA, ') - ', n.DESCRIZIONE) as snippet,
+        concat(ol.AZIENDA_NOME, ' ricerca: ', ol.TITOLO_LAVORO, ' per sede di ', ol.AZIENDA_CITTA, '(', ol.AZIENDA_PROVINCIA, ') - ', n.DESCRIZIONE) AS snippet,
 		DATA_INSERIMENTO AS data
 	FROM
 		`pariopp`.offertelavoro ol
-        LEFT JOIN `pariopp`.nazioni n ON ol.FK_NAZIONE = n.ID;
+        LEFT JOIN `pariopp`.nazioni n ON ol.FK_NAZIONE = n.ID
+		
+UNION
+
+	SELECT
+		ev.ID,
+		"eventi" AS nometab,
+		"Nuovo evento" AS news,
+		ev.TITOLO AS snippet
+		DATE_FORMAT(INSERITO_IL, "%Y-%m-%d") AS giorno
+	FROM
+		`pariopp`.agendaeventi ev;
 
 
 /*
