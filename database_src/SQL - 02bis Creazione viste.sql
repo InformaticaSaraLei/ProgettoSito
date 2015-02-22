fb054c28b4047a8c132352d88991694ac510101a
Antonio Bucciol <antonio@bucciol.org>
2015-02-22 23:32:09 +0100

    Modificata ulteriormente tab config + vista vRSSFEEDS

--- database_src/SQL - 02bis Creazione viste.sql
+++ database_src/SQL - 02bis Creazione viste.sql
@@ -2,31 +2,37 @@ DROP VIEW IF EXISTS pariopp.vRSSFEEDS;
 
 CREATE VIEW pariopp.vRSSFEEDS
 AS
- select 
-        ol.ID AS ID,
-        'offertelavoro' AS nometab,
-        concat('Nuova offerta di lavoro: ',ol.TITOLO_LAVORO, ' presso ', ol.AZIENDA_NOME)  AS news,
-        concat(ol.AZIENDA_NOME,
-                ' ricerca: ',
-                ol.TITOLO_LAVORO,
-                ' per sede di ',
-                ol.AZIENDA_CITTA,
-                '(',
-                ol.AZIENDA_PROVINCIA,
-                ') - ',
-                n.DESCRIZIONE) AS snippet,
-        ol.DATA_INSERIMENTO AS data,
-	ol.FONTE_LINK as Link
-    from
-	pariopp.offertelavoro ol
-        left join pariopp.nazioni n ON ol.FK_NAZIONE = n.ID 
-    union select 
+	select 
+		ol.ID AS ID,
+		'offertelavoro' AS nometab,
+		concat('Nuova offerta di lavoro: ',ol.TITOLO_LAVORO, ' presso ', ol.AZIENDA_NOME)  AS news,
+		concat(ol.AZIENDA_NOME,
+				' ricerca: ',
+				ol.TITOLO_LAVORO,
+				' per sede di ',
+				ol.AZIENDA_CITTA,
+				'(',
+				ol.AZIENDA_PROVINCIA,
+				') - ',
+				n.DESCRIZIONE) AS snippet,
+		ol.DATA_INSERIMENTO AS giorno,
+		concat((SELECT GET_WEBSITE_URI()),
+				(SELECT GET_WEBPAGE_OFFERTELAVORO_QUERY()),
+				ol.ID) as Link
+		
+		from
+			pariopp.offertelavoro ol
+			left join pariopp.nazioni n ON ol.FK_NAZIONE = n.ID 
+			
+	union select 
         ev.ID AS ID,
         'eventi' AS nometab,
         concat('Nuovo evento: ',TITOLO) AS news,
         ev.CONTENUTO AS snippet,
         date_format(ev.INSERITO_IL, '%Y-%m-%d') AS giorno,
-		concat((SELECT GET_WEBSITE_URI()), '/eventi/paginaEvento.php?id=',ev.ID) as Link
+		concat((SELECT GET_WEBSITE_URI()),
+				(SELECT GET_WEBPAGE_AGENDAEVENTI_QUERY()),
+				ev.ID) as Link
     from
         pariopp.agendaeventi ev;
 
--- database_src/SQL - 07 Caricamento dati Config.sql
+++ database_src/SQL - 07 Caricamento dati Config.sql
@@ -1,2 +1,7 @@
 
-insert into config values (1, "http://test01.dsi.unive.it");
+insert into config
+	(ID, WEBSITE_URI, WEBPAGE_OFFERTELAVORO_QUERY, WEBPAGE_AGENDAEVENTI_QUERY) 
+	values (1,
+			"http://test01.dsi.unive.it",
+			"/lavoro/index.php?odl_from=local&mode=detail&jobkey=",
+			"/eventi/paginaEvento.php?id=");
